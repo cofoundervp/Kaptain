192a75097c348954d137043440838470b27277cf
Vinh Dang <dtt.vinh@gmail.com>
2016-03-28 16:25:21 +0700

    Update boiler-plate code.

--- index.js
+++ index.js
@@ -2,4 +2,18 @@
 
 const Kaptain = module.exports = require('./lib/kaptain');
 
-// TODO: Start app if running independently here
+if (!module.parent) {
+    // NOTE: dotenv will look for .env file in the same directory with it
+    // Optionally, we can also import environment variables before starting node app without using dotenv
+    require('dotenv').config({silent: true});
+
+    var db_config = {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    };
+
+    const kaptain = new Kaptain(process.env.DB_DRIVER, process.env.DB_HOST,
+        process.env.DB_DATABASE, process.env.DB_PORT, db_config);
+
+    kaptain.listen(process.env.SERVER_PORT || 443);
+}
