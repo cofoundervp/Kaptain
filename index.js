4f48c8ac9fb266e8c66a9e395707967b05916d7c
Vinh Dang <dtt.vinh@gmail.com>
2016-04-12 10:47:55 +0700

    Update .env file. Add basic routes. Update mongodb datasource. Add a testing datasource.

--- index.js
+++ index.js
@@ -7,13 +7,9 @@ if (!module.parent) {
     // Optionally, we can also import environment variables before starting node app without using dotenv
     require('dotenv').config({silent: true});
 
-    var db_config = {
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD
-    };
+    var db_config = {};
 
-    const kaptain = new Kaptain(process.env.DB_DRIVER, process.env.DB_HOST,
-        process.env.DB_DATABASE, process.env.DB_PORT, db_config);
+    const kaptain = new Kaptain(process.env.DB_DRIVER, process.env.DB_URI, db_config);
 
     kaptain.listen(process.env.SERVER_PORT || 8080);
 }
