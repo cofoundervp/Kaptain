e29ac1dcc306ea91041c0bca6a18e0ddb35af72f
Vinh Dang <dtt.vinh@gmail.com>
2016-04-12 16:11:21 +0700

    Update kaptain config parameter.

--- index.js
+++ index.js
@@ -3,13 +3,20 @@
 const Kaptain = module.exports = require('./lib/kaptain');
 
 if (!module.parent) {
+    console.log('Running as independent module. Starting Kaptain...');
     // NOTE: dotenv will look for .env file in the same directory with it
     // Optionally, we can also import environment variables before starting node app without using dotenv
     require('dotenv').config({silent: true});
 
-    var db_config = {};
+    var config = {
+        client: true // use built-in kaptain UI
+    };
 
-    const kaptain = new Kaptain(process.env.DB_DRIVER, process.env.DB_URI, db_config);
+    const kaptain = new Kaptain(process.env.DB_URI, config);
 
-    kaptain.listen(process.env.SERVER_PORT || 8080);
+    kaptain.listen(process.env.SERVER_PORT || 8080, function() {
+        var server = this;
+        var port = server.address().port;
+        console.log('Listening on port %s', port);
+    });
 }
